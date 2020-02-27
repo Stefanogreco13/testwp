a7477f9e5bd3c8ff6d664432b85106ea881b0e34
Stefanogreco13 <stefano.greco1993@hotmail.it>
2020-02-27 23:30:43 +0100

    Update functions.php

--- wp-content/themes/twentytwenty/functions.php
+++ wp-content/themes/twentytwenty/functions.php
@@ -755,7 +755,3 @@ function twentytwenty_get_elements_array() {
 	*/
 	return apply_filters( 'twentytwenty_get_elements_array', $elements );
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
-function enqueue_parent_theme_style() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
